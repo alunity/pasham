43eef00702e1055a69ed9c8eec6ddf9294ae5301
alunity <75143943+alunity@users.noreply.github.com>
2023-01-04 20:23:20 +0000

    fixed file not being closed

--- src/grab_recipe.py
+++ src/grab_recipe.py
@@ -1,10 +1,10 @@
 import json
 from random import randint
+
+
 def getRandomRecipe():
     f = open('recipes.json')
-    x = randint(0,3)
     recipes = json.load(f)
-    chosenRecipe = recipes[x]
-    return chosenRecipe
+    chosenRecipe = recipes[randint(0, len(recipes))]
     f.close()
-getRandomRecipe()
+    return chosenRecipe
